f1a6a1fe0d5c61d19bdc79edcad7f57d31cee41a
Joel Berghoff <joel.berghoff@glassdoor.com>
2017-02-21 13:40:55 -0800

    Updated About page a little.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,14 @@ var React = require('react');
 var About = (props) => {
     return (
       <div>
-        <h3>About</h3>
+        <h1 className="text-center">About</h1>
+          <p>This is a weather application built on react. I am creating this within the Udacity course.</p>
+            <p>Here are some tools I've used</p>
+            <ul>
+              <li>
+                <a href="https://facebook.github.io/rect">React</a> - This is React suckas
+              </li>
+            </ul>
       </div>
     );
   }
