56c7efd09225752fdb6e416daaacb9e1bb7ae84c
Joel Berghoff <joel.berghoff@glassdoor.com>
2017-02-21 13:46:22 -0800

    Updated weather page.

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -4,7 +4,7 @@ var PropTypes = React.PropTypes;
 var WeatherMessage = ({temp, loc}) => {
     return (
       <div>
-        <h3>It is {temp} degrees in {loc} </h3>
+        <h3 className="text-center">It is {temp} degrees in {loc} </h3>
       </div>
     );
   }
