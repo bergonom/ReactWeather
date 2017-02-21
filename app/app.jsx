c0f7a07b87b618b3873e05aa74a0c6c17a43df80
Joel Berghoff <joel.berghoff@glassdoor.com>
2017-02-21 12:27:52 -0800

    Trying to get jquery and foundation to work.

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,11 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+// Load Foundation
+// @TODO: What do style! and css! do?
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
